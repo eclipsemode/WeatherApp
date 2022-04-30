7ea46e1a91236e6479eb6e0a1d709b456f8ccc66
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-04-30 22:23:11 +0300

    WeatherApp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '56d13c7ab0msh213e76547477112p14072bjsn79530b568002',
 };
 
 /*
